7b3277191b0789f6389ea3cc5f17b720bbd02a1d
Ruifeng Zhang <failury29@gmail.com>
2021-10-28 17:46:35 -0400

    signin/signup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import ClippedDrawer from "./layouts/Dashboard";
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-   <ClippedDrawer/>
+    <main>
+      <Switch>
+      <Route path="/" component={ClippedDrawer} exact />
+                <Route path="/signin" component={SignIn} />
+                <Route path="/signup" component={SignUp} />
+      </Switch>
+    </main>
+
+  
   );
 }
 
