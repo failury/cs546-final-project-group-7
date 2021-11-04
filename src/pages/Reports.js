f91c572f47ca2b84e5e8311c9f85c5629740eb18
kai-w0 <19853243280@163.com>
2021-11-03 22:12:21 -0400

    main

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Reports from '../components/Reports';
 
-export default function Reports() {
+export default function report() {
     return (
-        <div>
-            <h1>Reports</h1>
-        </div>
+        <>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <Box
+                    component="main"
+                    sx={{
+                        backgroundColor: (theme) =>
+                            theme.palette.mode === 'light'
+                                ? theme.palette.grey[100]
+                                : theme.palette.grey[900],
+                        flexGrow: 1,
+                        height: '100vh',
+                        overflow: 'auto',
+                    }}
+                >
+                    <Toolbar />
+                    <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                        <Grid container spacing={3}>
+                            {/* Reports */}
+                            <Grid item xs={12} md={8} lg={9}>
+                                <Paper
+                                    sx={{
+                                        p: 2,
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        height: 360,
+                                    }}
+                                >
+                                    <Reports/>
+                                </Paper>
+                            </Grid>
+                        </Grid>
+                    </Container>
+                </Box>
+            </Box>
+        </>
     )
 }
