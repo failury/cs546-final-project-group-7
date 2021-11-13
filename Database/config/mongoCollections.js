aa9a0c345c0160ed67b0d047c49f6727e6a5fa16
Ruifeng Zhang <failury29@gmail.com>
2021-11-13 00:19:15 -0500

    data

---
+++ Database/config/mongoCollections.js
@@ -0,0 +1,25 @@
+const dbConnection = require('./mongoConnection');
+
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection(); 
+      _col = await db.collection(collection);
+    } 
+
+    return _col;
+  };
+};
+
+module.exports = {
+  users: getCollectionFn('users'),
+  wallet: getCollectionFn('wallet'),
+  schedule_payments: getCollectionFn('schedule_payments'),
+  budget: getCollectionFn('budget'),
+  transaction: getCollectionFn('transaction'),
+  category: getCollectionFn('category'),
+  currency: getCollectionFn('currency') 
+
+};
