81a82fb9ea8bfa45c4819a56e7a0ca376db8d887
kai-w0 <19853243280@163.com>
2021-12-01 18:57:20 -0500

    main

--- Database/data/budget.js
+++ Database/data/budget.js
@@ -1,46 +1,121 @@
 const mongoCollections = require('../config/mongoCollections');
-const budgetCollection = mongoCollections.budget
+const budgetCollection = mongoCollections.budget;
+const users = require('./users');
+const categories = require('./category');
 let { ObjectId } = require('mongodb');
 
-async function create(name, amt, budgetType){
-    const budget_collection = await budgetCollection();
+// async function create(name, amt, budgetType){
+//     const budget_collection = await budgetCollection();
 
-    let newBudget = { 
-        name: name,
-        amt: amt,
-        budgetType: budgetType
-    }; 
+//     let newBudget = { 
+//         name: name,
+//         amt: amt,
+//         budgetType: budgetType
+//     }; 
 
-    const insertinfo = await budget_collection.insertOne(newBudget)
-    const newId = insertinfo.insertedId;
-    const new_budget = await budget_collection.findOne({ _id: newId });
-    const newObjId = ObjectId(new_budget._id); 
-    let x = new_budget._id
-    x = newObjId.toString(); 
-    new_budget._id=x;
+//     const insertinfo = await budget_collection.insertOne(newBudget)
+//     const newId = insertinfo.insertedId;
+//     const new_budget = await budget_collection.findOne({ _id: newId });
+//     const newObjId = ObjectId(new_budget._id); 
+//     let x = new_budget._id
+//     x = newObjId.toString(); 
+//     new_budget._id=x;
 
-    return(new_budget)
-}
+//     return(new_budget)
+// }
 
-async function getAll(){
-    let list = [];
-    const budget_Collection = await budgetCollection();
-    const budgetList = await budget_Collection.find({}).toArray();
+// async function getAll(){
+//     let list = [];
+//     const budget_Collection = await budgetCollection();
+//     const budgetList = await budget_Collection.find({}).toArray();
 
-    for(let i=0; i<budgetList.length; i++){
-        let x = budgetList[i]._id
-        let y=x.toString();
-        budgetList[i]._id = y;
+//     for(let i=0; i<budgetList.length; i++){
+//         let x = budgetList[i]._id
+//         let y=x.toString();
+//         budgetList[i]._id = y;
+//     }
+
+//     for(let i=0;i<budgetList.length;i++){
+//         let ls = {"budget name": budgetList[i].name, "Amt": budgetList[i].amt, "budget_Type": budgetList[i].budgetType};
+//         list.push(ls);
+//     }
+//     return list;
+// }
+
+// module.exports = {
+//     create,
+//     getAll
+// }
+
+
+let exportedMethods = {
+    async get(id){
+        if (!id) throw 'You must provide an id to search for';
+        if (typeof id != 'string') throw "the id must be a string";
+        if (id.trim().length === 0) throw "the id is empty spaces";
+
+        let parsedId = ObjectId(id);
+        if (!parsedId) throw "cannot parse id";
+
+        const budgetCollection = await budget();
+        let budget = await budgetCollection.findOne({_id:parsedId});
+        if (budget === null) throw 'No such budget';
+
+        return budget;
+    },
+    async create(username,budgetname,amount,category,type){
+        const budgetCollection = await budget();
+        let newBudget = {
+            username:username,
+            budgetname:budgetname,
+            amount:amount,
+            category:category,
+            type:type
+        }
+        let insertInfo = await budgetCollection.insertOne(newBudget);
+        await users.addBudgetToUser(username,budgetname,amount,category,type);
+        await categories.addBudgetToCategory(category,budgetname,amount,type);
+        let newId = insertInfo.insertedId;
+        let new_budget = await budgetCollection.findOne({_id:newId});
+        return new_budget;
+    },
+    async update(id,updateBudget) {
+        let parsedId = ObjectId(id);
+        if (!parsedId) throw "cannot parse id";
+
+        const budgetCollection = await budget();
+        const updateBudgetInfo = {
+            username: updateBudget.username,
+            budgetname: updateBudget.budgetname,
+            amount: updateBudget.amount,
+            category: updateBudget.category,
+            type: updateBudget.type
+        }
+
+        const updateInfo = await budgetCollection.updateOne(
+            {id: parsedId},
+            {$set: updateBudgetInfo}
+        )
+        if (updatedInfo.modifiedCount === 0) {
+            throw 'could not update restaurant successfully';
+        }
+
+        return await this.get(id);
+    },
+    async delete(id){
+        if (!id) throw 'You must provide an id to search for';
+        if (typeof id != 'string') throw "the id must be a string";
+        if (id.trim().length === 0) throw "the id is empty spaces";
+
+        let parsedId = ObjectId(id);
+        if (!parsedId) throw "cannot parse id";
+
+        const budgetCollection = await budget();
+        let deleteInfo = await budgetCollection.deleteOne({_id:parsedId});
+        if (deleteInfo.deletedCount === 0) throw 'Cannot delete this budget';
+
+        return { deleted: true };
     }
-
-    for(let i=0;i<budgetList.length;i++){
-        let ls = {"budget name": budgetList[i].name, "Amt": budgetList[i].amt, "budget_Type": budgetList[i].budgetType};
-        list.push(ls);
-    }
-    return list;
 }
 
-module.exports = {
-    create,
-    getAll
-}
+module.exports = exportedMethods;
--- Database/routes/budget.js
+++ Database/routes/budget.js
@@ -1,33 +1,66 @@
 const express = require('express');
 const router = express.Router();
-const data = require('../data');
-const budgetdata = data.budget;
+const budgets = require('../data');
+const budgetData = budgets.budget;
 
-router.get('/budget', async (req, res) => {
+router.get('/budget/:id', async (req, res) => {
     try {
-        let budgetlist = await budgetdata.getAll();
-        res.json(budgetlist);
+        let budget = await budgetData.get(req.params.id);
+        res.json(budget);
     } catch (e) {
-        console.log(e);
+        res.status(404).json({ error: 'budget not found' });
+    }
+});
+
+router.post('/budget',async (req,res) => {
+    let budgetInfo = req.body;
+    try {
+        let newBudget = await budgetData.create(
+            budgetInfo.username,
+            budgetInfo.budgetname,
+            budgetInfo.amount,
+            budgetInfo.category,
+            budgetInfo.type
+        );
+        res.json(newBudget);
+    } catch (e) {
         res.sendStatus(500);
     }
 });
 
-router.post('/budget', async (req,res) => {
+router.patch('/budget/:id',async (req,res) => {
     let budgetInfo = req.body;
 
     try {
-        const newBudget = await budgetdata.create(
-            budgetInfo.name,
-            budgetInfo.amt,
-            budgetInfo.budgetType, 
-        );
-        res.json(newBudget);
+        await budgetData.get(req.params.id);
     } catch (e) {
-        console.log(e);
-        res.sendStatus(500);
+        res.status(404).json({ error: 'Budget not found' });
+        return;
     }
 
+    try {
+        let updatedBudget = await budgetData.update(req.params.id,budgetInfo);
+        res.json(updatedBudget);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
+
+router.delete('/budget/:id', async (req, res) => {
+    if (!req.params.id) throw 'You must specify an ID to delete';
+    try {
+        await budgetData.get(req.params.id);
+    } catch (e) {
+        res.status(404).json({ error: 'Budget not found' });
+        return;
+    }
+
+    try {
+        await budgetData.delete(req.params.id);
+        res.sendStatus(200);
+    } catch (e) {
+        res.sendStatus(500);
+    }
 });
 
 module.exports = router;
