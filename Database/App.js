e78c5189fbccfabd966230448556d125fc44d408
harsh_5670 <hrvadchhak@icloud.com>
2021-12-04 15:34:37 -0500

    search

--- Database/App.js
+++ Database/App.js
@@ -1,21 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const configRoutes = require('./routes');
-const {client} = require('./config/mongoConnection')
-const users = require('./data/users')
-const wallet = require('./data/wallet')
-const schedule_payments = require('./data/schedule_payments')
-const budget = require('./data/budget')
-const transaction = require('./data/transaction')
-const category = require('./data/category')
-const currency = require('./data/currency')
+const configRoutes = require("./routes");
+const { client } = require("./config/mongoConnection");
+const users = require("./data/users");
+const wallet = require("./data/wallet");
+const schedule_payments = require("./data/schedule_payments");
+const budget = require("./data/budget");
+const transaction = require("./data/transaction");
+const category = require("./data/category");
+const currency = require("./data/currency");
 
-const closeConnection = require('./config/mongoConnection')
-app.use(express.json()); 
+const closeConnection = require("./config/mongoConnection");
+app.use(express.json());
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
 const generateAccessToken = (user) => {
@@ -24,33 +24,37 @@ const generateAccessToken = (user) => {
   });
 };
 
-
-
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
   try {
-    var user = await users.checklogin(username,password);
+    var user = await users.checklogin(username, password);
     const accessToken = generateAccessToken(user);
     res.send({
-      token: accessToken
+      token: accessToken,
     });
   } catch (error) {
     return res.status(400).json(error.message);
   }
 });
 
-app.use('/signup', async (req, res) => {
+app.use("/signup", async (req, res) => {
   let firstname = req.body.firstname;
   let lastname = req.body.lastname;
   let username = req.body.username;
   let password = req.body.password;
   let url = req.body.url;
   try {
-    let newuser = await users.create(firstname,lastname,username,password,url);
-    res.send({'newuser': newuser.username});
+    let newuser = await users.create(
+      firstname,
+      lastname,
+      username,
+      password,
+      url
+    );
+    res.send({ newuser: newuser.username });
   } catch (error) {
-    return res.status(400).send('register failed');
+    return res.status(400).send("register failed");
   }
 });
 
@@ -60,17 +64,15 @@ app.use('/signup', async (req, res) => {
 //     let id = await jwt.verify(token, "mySecretKey", (err, result) => {
 //       res.json({'userid': result.id});
 //     });
-    
+
 //   } catch (error) {
 //     res.send('no authenticated');
 //   }
-  
-// });
 
+// });
 
 configRoutes(app);
 
 app.listen(2000, () => {
-  console.log('Backend Server is RUNNING on http://localhost:2000');
+  console.log("Backend Server is RUNNING on http://localhost:2000");
 });
-
